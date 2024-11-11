ad36b920a7ce782d23aea6f7596ddbc38610a9aa
jeenskim <143062368+jeenskim@users.noreply.github.com>
2024-11-11 09:41:42 -0500

    Update ROM_jax.py

--- ROM_jax.py
+++ ROM_jax.py
@@ -9,6 +9,8 @@ from jax import config
 import optax
 from tqdm import tqdm
 from Parameters_jax import tsteps, Rnum, dx, dt, seq_num
+import pickle
+import csv
 
 config.update("jax_enable_x64", True)
 
@@ -21,7 +23,7 @@ from Parameters import K,M, spatial_resolution
 from Compression import field_compression, nonlinear_compression
 from Problem import collect_snapshots_field, collect_snapshots_nonlinear
 from Plotting import plot_coefficients, plot_gp, plot_comparison
-from GP_jax import gp_evolution, MLP
+from GP_jax import gp_evolution, MLP, gp_evolution_ML
 
 
 
@@ -31,14 +33,11 @@ from GP_jax import gp_evolution, MLP
 def Dataloader(data,batch_size,batch_time):
     time_chunks = []
     for i in range(data.shape[1] - batch_time):
-        #time_chunks.append(data[i:i+batch_time])
         time_chunks.append(data[:,i:i+batch_time])
-        #print(data[:,i:i+batch_time].shape)
-    # print(len(time_chunks))
+
     extra = len(time_chunks) % batch_size
     time_chunks = np.array(time_chunks[:-extra])
     split = np.random.permutation(np.array(np.split(time_chunks,time_chunks.shape[0]//batch_size)))
-    # print(split.shape)
     return split
 
 
@@ -51,19 +50,18 @@ def Train(model, gp_rom_train, train_data, test_data, lr, epochs, name='try'):
 
 
     def loss(params,batch):
-            preds=gp_rom_train.pod_deim_ML_evolve(params, batch[:, 0])
+            preds, _=gp_rom_train.pod_deim_ML_evolve(params, batch[:, 0])
 
-            #print (preds.shape)
-            #exit()
-            #preds = model(params,batch[0])
             L = jnp.mean((jnp.abs(preds[:,1:] - batch[:,1:])))
-            return jnp.linalg.norm(preds[:,1:])
+            #L = jnp.mean((jnp.square(preds[:,1:] - batch[:,1:])))
+            
+            return L
         
 
     vloss =  lambda params,batch :jnp.mean(jax.vmap(loss,in_axes=(None,0))(params,batch))
     
     
-    #@jax.jit
+    @jax.jit
     def step(params, opt_state, batch):
         loss_value, grads = jax.value_and_grad(vloss,argnums=0)(params,batch)
         updates, opt_state = optimizer.update(grads, opt_state, params)
@@ -71,20 +69,23 @@ def Train(model, gp_rom_train, train_data, test_data, lr, epochs, name='try'):
         return params, opt_state, loss_value, grads
     
     
-
-    for i in range(1,epochs+1): #tqdm(range(epochs)):#
-        losses = []
-        for batch in tqdm(train_data): # train_data:#
-            params, opt_state, loss_value, grads = step(params,opt_state,batch)
-            print(grads)
-            losses.append(loss_value)
-        net_loss = np.mean(np.array(losses))
-        print(net_loss)
-        #test_loss = vloss(params,test_data[np.random.randint(len(test_data))])
-        #print(f'net_loss: {net_loss}, test_loss: {test_loss}')
-        #if test_loss < best_loss:
-        #    best_loss = test_loss
-        #    pickle.dump(params,open(f'params/{name}','wb'))
+    with open(f"{name}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["Epoch", "Loss"]) 
+        for i in range(1,epochs+1): #tqdm(range(epochs)):#
+            losses = []
+            for batch in train_data: # train_data:#
+                params, opt_state, loss_value, grads = step(params,opt_state,batch)
+                losses.append(loss_value)
+            net_loss = np.mean(np.array(losses))
+            pickle.dump(params,open(f'params/{name}','wb'))
+            print(f'epochs={i}, net_loss={net_loss}')
+            writer.writerow([i, net_loss])
+            #test_loss = vloss(params,test_data[np.random.randint(len(test_data))])
+            #print(f'net_loss: {net_loss}, test_loss: {test_loss}')
+            #if test_loss < best_loss:
+            #    best_loss = test_loss
+            #    pickle.dump(params,open(f'params/{name}','wb'))
 
 
         
@@ -104,6 +105,7 @@ if __name__ == "__main__":
     V, Ytilde = field_compression(Ytot,K) # K is the number of retained POD bases for field (and also dimension of reduced space)
     U, Ftilde_exact, P = nonlinear_compression(V,Ftot,M) # M is the number of retained POD basis for nonlinear term
 
+    print(U.shape)
     
     indices = np.zeros((P.shape[1]), dtype=int)
 
@@ -112,76 +114,50 @@ if __name__ == "__main__":
     
     np.save('sampling_index_DEIM.npy',indices)
     
+    
     # Initialize ROM class
     ytilde_init = Ytilde[:,0].reshape(np.shape(Ytilde)[0],1)
-    # gp_rom = gp_evolution(V,U,P,ytilde_init)
+    gp_rom = gp_evolution(V,U,P,ytilde_init)
+    
+    
+    # ROM assessments - DEIM
+    gp_rom.pod_deim_evolve()
+    Ytilde_pod_deim = np.copy(gp_rom.state_tracker)
 
-    # #Filter exact for stability
-    # Ftilde_filtered = np.copy(Ftilde_exact)
-    # for i in range(K):
-    #     Ftilde_filtered[i,:] = savgol_filter(Ftilde_exact[i,:],65,2)
-    #     #Ftilde_filtered=Ftilde_filtered.at[i,:].set(savgol_filter(Ftilde_exact[i,:],65,2))
-    # # Plot comparison of filtered and exact nonlinear term modes
-    # plot_gp(Ftilde_exact,'Exact',Ftilde_filtered,'Filtered')
-
-    #  # ROM assessments - GP
-    # gp_rom.pod_gp_evolve()
-    # Ytilde_pod_gp = np.copy(gp_rom.state_tracker)
-    # # Plot comparison of POD-GP and truth
-    # plot_gp(Ytilde,'True',Ytilde_pod_gp,'POD-GP')
+    np.save('POD_DEIM_test.npy',np.matmul(V, Ytilde_pod_deim))
+    
     
     # ROM assessments - DEIM-ML
-    
-    layer_sizes = [spatial_resolution, 50, 50, 50, spatial_resolution]
-    model = MLP(1536)
-    sampling_factor = 10
-    #gp_rom.set_ML_sampling(10)
+    model = MLP(3072)
+    sampling_factor = U.shape[1]
+
     
     
-    batch_size = 60
-    batch_time = 10
+    batch_size = 16
+    batch_time = 2
+    lr = 1e-5
     train_data = Dataloader(Ytilde, batch_size, batch_time)
     
-    #num_steps = int(jnp.shape(tsteps)[0]-1)
     num_steps = batch_time    
     state_tracker = jnp.zeros(shape=(jnp.shape(ytilde_init)[0],num_steps+1),dtype='double')
     state_tracker = state_tracker.at[:, 0].set(ytilde_init[:, 0])
     nl_state_tracker = jnp.zeros(shape=(jnp.shape(ytilde_init)[0],num_steps+1),dtype='double')
       
       
-    gp_rom_train = gp_evolution(V,U,P,batch_time, model, sampling_factor)
-    # gp_rom_train.num_steps = batch_time
-    # gp_rom_train.state_tracker = jnp.zeros(shape=(jnp.shape(ytilde_init)[0],gp_rom_train.num_steps),dtype='double')
-    # gp_rom_train.nl_state_tracker = jnp.zeros(shape=(jnp.shape(ytilde_init)[0],gp_rom_train.num_steps),dtype='double')
-    # gp_rom_train.set_ML_sampling(10)
-    # gp_rom_train.k_index_tracker = jnp.zeros(shape=(int(jnp.shape(gp_rom_train.V)[0]/gp_rom_train.sampling_factor),gp_rom_train.num_steps+1),dtype='int')
-
-
-    Train(model, gp_rom_train, train_data, train_data, lr=1e-4, epochs=10, name='try')
+    name = f'batchtime_{batch_time}_lr_{lr:.1e}'
+    
+    gp_rom_ml_train = gp_evolution_ML(V,U,P,batch_time, model, sampling_factor, train=True)
+    Train(model, gp_rom_ml_train, train_data, train_data, lr=lr, epochs=2500, name=name)
     
     
-    # sampling_index = jnp.copy(gp_rom_train.k_index_tracker)
-    # np.save('sampling_index_DEIM_ML_train.npy', sampling_index)
-        
+    # model = MLP(3072)
+    # params = pickle.load(open(f'params/{name}','rb'))
+    # gp_rom_ml_test = gp_evolution_ML(V,U,P,1000, model, sampling_factor, train=False)
+    # Ytilde_pod_deim_ML, sampling_index   = gp_rom_ml_test.pod_deim_ML_evolve(params, ytilde_init[:, 0])
     
 
-    
-
-    # gp_rom.pod_deim_ML_evolve(model)
-    # Ytilde_pod_deim_ML = jnp.copy(gp_rom.state_tracker)
-    
-    
-    
-    
-    
-    # plot_gp(Ytilde,'True',Ytilde_pod_deim_ML,'POD-DEIM_ML')
-    
-    # sampling_index = jnp.copy(gp_rom.k_index_tracker)
-    # print(sampling_index)
-    # #print(sampling_index.shape)
-
-    # np.save('sampling_index_DEIM_ML.npy', sampling_index)
-    # np.save('POD_DEIM_ML.npy',np.matmul(V, Ytilde_pod_deim_ML))
+    # np.save(f'sampling_index_DEIM_ML_{name}.npy', sampling_index)
+    # np.save(f'POD_DEIM_ML_{name}.npy',np.matmul(V, Ytilde_pod_deim_ML))
     
     
 
@@ -190,8 +166,4 @@ if __name__ == "__main__":
     
     
     
-
-
-
-
 
